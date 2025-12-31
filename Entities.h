a24e8afb154ea9f943b344000395be7691821666
Arzu Bal <arzubbal@gmail.com>
2026-01-01 00:32:18 +0300

    Add files via upload

---
+++ Entities.h
@@ -0,0 +1,41 @@
+#ifndef ENTITIES_H
+#define ENTITIES_H
+
+#include <QString>
+
+// --- PERSON 2: PET ENTITY ---
+class Pet {
+public:
+    int id;
+    QString name;
+    QString species;
+    int age;
+    QString health;
+    QString status; // "Available" veya "Adopted"
+
+    Pet(int i, QString n, QString s, int a, QString h, QString st = "Available")
+        : id(i), name(n), species(s), age(a), health(h), status(st) {}
+};
+
+// --- PERSON 3: ADOPTER ENTITY ---
+class Adopter {
+public:
+    int id;
+    QString fullName;
+    QString phone;
+    QString address;
+
+    Adopter(int i, QString fn, QString ph, QString addr)
+        : id(i), fullName(fn), phone(ph), address(addr) {}
+};
+
+// --- PERSON 1: STAFF ENTITY (NEW) ---
+class Staff {
+public:
+    QString username;
+    QString password;
+
+    Staff(QString u, QString p) : username(u), password(p) {}
+};
+
+#endif // ENTITIES_H
